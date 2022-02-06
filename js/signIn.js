e4f8bd83e0846cdad0b863e32b360a92008c0f23
rossy <h2tafemi@gmail.com>
2022-02-06 16:00:21 +0900

    signIn mod

--- js/signIn.js
+++ js/signIn.js
@@ -8,3 +8,18 @@ async function signIn() {
     }
 }
 
+var logInButton = document.getElementById("logInButton");
+if (logInButton) {
+    logInButton.addEventListener("click", function () {
+        Auth.signIn(
+            document.getElementById("email").value,
+            document.getElementById("password").value
+        ).then(() => {
+            console.log("Log In Succeed!");
+            window.location.href = "soluvision/index.html";
+        });
+    });
+}
+
+
+
